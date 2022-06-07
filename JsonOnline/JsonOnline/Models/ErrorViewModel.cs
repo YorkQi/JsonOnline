654da991accb9d7cbd99d31a493cec917ab079af
YorkNow <qiyao@51testing.com>
2022-06-07 16:29:51 +0800

    初始化

---
+++ JsonOnline/JsonOnline/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JsonOnline.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
